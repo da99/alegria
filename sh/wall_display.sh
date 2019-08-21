50ec2ad34bd4e4368dc10f567e6379b3821f9bba
da_alegria <da+alegria@ihatespam.miniuni.com>
2019-08-20 22:41:34 -0500

    Update before running.

--- sh/wall_display.sh
+++ sh/wall_display.sh
@@ -3,11 +3,13 @@
 #
 set -u -e -o pipefail -x
 
+cd $(dirname "$(realpath "$0")")/..
+git pull || :
+
 cd $(dirname "$(realpath "$0")")
 
 
 killall lemonbar || :
-xset s 6000 6000 6000 || :
 killall mpv || :
 killall smplayer || :
 killall vlc || :
@@ -17,29 +19,22 @@ for x in $(wmctrl -l | cut -d' ' -f1); do
   xdotool windowminimize "$x" || :
 done
 
-top_msg="%{c}Follow us on Facebook. We're also on Instagram: @AlegriaGrillKaty"
+top_msg="%{c}Follow us on Facebook: AlegriaGrill. We're also on Instagram: @AlegriaGrillKaty"
 blue="#132492"
 white="#ffffff"
 black="#000000"
 orange="#FF5E00"
 
 set -x
-(
-echo "%{c}Follow us on Facebook: AlegriaGrill. We're also on Instagram: @AlegriaGrillKaty" | $HOME/progs/lemonbar-xft/lemonbar -p -n "random_photo_caption" \
-	-f "helv:size=28:antialias=true" \
-	-g "1920x80+0+0" \
-	-B "#132492" \
-	-F "#ffffff" || :
-) &
 
 # TOP CAPTION:
-( echo $top_msg | ./bar -n "top_photo_caption" -B $blue -F $white || : ) &
+( echo $top_msg | ./bar.sh -n "top_photo_caption" -B $blue -F $white || : ) &
 
 # Middle Caption:
 ( ./middle.caption.sh || : ) &
 
 # BOTTOM CAPTION:
-( ./bottom.caption.sh | lemonbar -n "bottom_photo_caption" -B $blue -F $black -b || : ) &
+( ./bottom.caption.sh | ./bar.sh -n "bottom_photo_caption" -B $blue -F $white -b || : ) &
 
 
 # Photo Loop:
