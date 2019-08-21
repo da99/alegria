19a46bc05b5e63208313280cfa9a9ac77ff00ea6
da99 <ihatespam@miniuni.com>
2019-08-21 08:53:10 -0500

    Fixed: kill caption when it no longer needed.

--- sh/middle.caption.sh
+++ sh/middle.caption.sh
@@ -31,13 +31,15 @@ while true ; do
   esac
   closing_hour24="$((12 + $closing_hour))"
 
-  is_closed=""
 
   if test $hour24 -lt 11; then
     ( echo "%{c}Alegria has not opened yet." | ./bar.sh -n middle_bar_caption -g "1920x80+0+$((1080/2 - 40))" -B "#AC0000" -F "#ffffff" ) &
     sleep $(( ((11 - $hour24) * 60) + ((60 - $min) * 60)  )) || sleep 1
+    pkill -f middle_bar_caption
     continue
   fi
+
+  is_closed=""
 
   if test $min -eq 59 && test $((closing_hour24 - 1)) -eq $hour24 ; then
     is_closed="yes"
