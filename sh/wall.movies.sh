5bfc8fb97df746d53a0f387ba151397350220972
da99 <ihatespam@miniuni.com>
2019-08-24 15:42:25 -0500

    Development checkpoint.

--- sh/wall.movies.sh
+++ sh/wall.movies.sh
@@ -6,8 +6,10 @@ set -u -e -x
 cd $HOME/apps/alegria
 
 while true ; do
-  sleep $(( 60 * 15))
+  sh/sleep.minute
   if sh/is.open ; then
     mpv --fullscreen $HOME/Videos/00-alegria_comm.mp4
   fi
+
+  sleep $(( 60 * 15 ))
 done
