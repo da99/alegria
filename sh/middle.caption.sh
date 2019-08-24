affc71e5ce7e4cf65d0084129d509385400508c4
da99 <ihatespam@miniuni.com>
2019-08-24 11:13:03 -0500

    Refactor.

--- sh/middle.caption.sh
+++ sh/middle.caption.sh
@@ -3,57 +3,27 @@
 #
 set -u -e -o pipefail -x
 
-cd $(dirname "$(realpath "$0")")
+cd $HOME/apps/alegria
 
 while true ; do
 
-  day_name="$(date +"%a")"
-  hour="$(date +"%I")"
-  hour24="$(date +"%H")"
-  min="$(date +"%M")"
-  second="$(date +"%S")"
-
-  public_time="$(date +"%I:%M %p")"
-
-  case $day_name in
-    Sun)
-      closing_hour="4"
-      ;;
-    Mon)
-      closing_hour="3"
-      ;;
-    Fri|Sat)
-      closing_hour="9"
-      ;;
-    *)
-      closing_hour="8"
-      ;;
-  esac
-  closing_hour24="$((12 + $closing_hour))"
-
-
-  if test $hour24 -lt 11; then
+  if sh/is.opening ; then
     ( echo "%{c}Alegria has not opened yet." | ./bar.sh -n middle_bar_caption -g "1920x80+0+$((1080/2 - 40))" -B "#AC0000" -F "#ffffff" ) &
-    sleep $(( ((11 - $hour24) * 60) + ((60 - $min) * 60)  )) || sleep 1
+    while ! sh/is.opening ; do
+      sh/sleep.minute
+    done
     pkill -f middle_bar_caption || :
     continue
   fi
 
-  is_closed=""
-
-  if test $min -eq 59 && test $((closing_hour24 - 1)) -eq $hour24 ; then
-    is_closed="yes"
-  fi
-
-  if test $hour24 -ge $closing_hour24 || test $hour24 -lt 11; then
-    is_closed="yes"
-  fi
-
-  if ! test -z $is_closed ; then
+  if sh/is.closed ; then
     ( echo "%{c}Alegria is closed. No more orders." | ./bar.sh -n middle_bar_caption -g "1920x80+0+$((1080/2 - 40))" -B "#AC0000" -F "#ffffff" ) &
-    sleep $((60 * 60))
+    while sh/is.closed ; do
+      sh/sleep.minute
+    done
     pkill -f middle_bar_caption || :
+    continue
   fi
 
-  sleep $((60 - $second)) || sleep 1
+  sh/sleep.minute
 done # while
