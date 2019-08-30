09ae4554c55b450c16bb02e9d0fe8369bc4b3078
da_alegria <da+alegria@ihatespam.miniuni.com>
2019-08-29 19:55:35 -0500

    Delete all ugly photos.

--- sh/bar.sh
+++ sh/bar.sh
@@ -7,5 +7,6 @@ PATH="$PATH:$HOME/progs/lemonbar-xft"
 exec lemonbar -p  \
 	-f "helv:size=32:antialias=true" \
   -f "helv:size=48:weight=bold:antialias=true" \
+  -f "helv:size=40:weight=bold:antialias=true" \
 	-g "1920x80+0+0" \
   $@
