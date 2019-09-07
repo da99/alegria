13595c1b51425e14dd5247b62b8bd0ceef7f759c
da99 <ihatespam@miniuni.com>
2019-09-07 11:05:54 -0500

    Development checkpoint.

--- sh/bar.sh
+++ sh/bar.sh
@@ -5,7 +5,7 @@ set -u -e
 
 PATH="$PATH:$HOME/progs/lemonbar-xft"
 exec lemonbar -p  \
-	-f "helv:size=32:antialias=true" \
+	-f "helv:size=32:weight=bold:antialias=true" \
   -f "helv:size=40:weight=bold:antialias=true" \
   -f "helv:size=48:weight=bold:antialias=true" \
 	-g "1920x80+0+0" \
